ed624abd23b506d8b5e54f965f1098f3587ce8ed
Min <85483551+min-coding@users.noreply.github.com>
2022-06-07 11:42:32 +0700

    replace state with reducer and add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,34 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen';
 import ProductScreen from './pages/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
-  /**
-   * Is the container of every page
-   */
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header className="App-header">
-          <Link to={'/'}>Homepage</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand> Minnisota</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/products/:slug" element={<ProductScreen />} />
-        </Routes>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/products/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        products: action.payload,
+        loading: false,
+      };
+    case 'FETCH_FAIL':
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
 
 export default function HomeScreen() {
-  const [products, setProducts] = React.useState([]);
+  const [{ loading, error, products }, dispatch] = React.useReducer(
+    logger(reducer),
+    {
+      products: [],
+      loading: true,
+      error: '',
+    }
+  );
 
   React.useEffect(() => {
     async function fetchData() {
-      const response = await axios.get('/api/products');
-      setProducts(response.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const response = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: response.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
     }
     fetchData();
   }, []);
@@ -16,28 +53,34 @@ export default function HomeScreen() {
   return (
     <>
       <h2> Featured product</h2>
-      <div className="featured-product-list">
-        {products.map((product) => {
-          return (
-            <div key={product.slug} className="product-container">
-              <Link to={`/products/${product.slug}`}>
-                <img
-                  className="product-image"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </Link>
-
-              <h3>
+      {loading ? (
+        <h1> Loading..........</h1>
+      ) : error ? (
+        <div> {error}</div>
+      ) : (
+        <div className="featured-product-list">
+          {products.map((product) => {
+            return (
+              <div key={product.slug} className="product-container">
                 <Link to={`/products/${product.slug}`}>
-                  <strong>{product.name}</strong>
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt={product.name}
+                  />
                 </Link>
-              </h3>
-              <h4>${product.price}</h4>
-            </div>
-          );
-        })}
-      </div>
+
+                <h3>
+                  <Link to={`/products/${product.slug}`}>
+                    <strong>{product.name}</strong>
+                  </Link>
+                </h3>
+                <h4>${product.price}</h4>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
