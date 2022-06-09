a852e3a2d17ba283b6ca24eecc7572acc411dc0a
Min <85483551+min-coding@users.noreply.github.com>
2022-06-09 15:17:25 +0700

    Finish add to cart and remove from cart

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,24 +3,35 @@ import React from 'react';
 const Store = React.createContext();
 const initialState = {
   cart: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
   },
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case 'CART_ADD_ITEM':
-      const newItem = action.payload
-      const existItem = state.cart.cartItems.find((item) => item._id === newItem._id);
-      const cartItems = existItem ? state.cart.cartItems.map(item => 
-        // Update item (if already in the cart)
-        item._id === existItem._id ? newItem : item 
-      ) :
-        // Add new item (if not in the cart)
-        [...state.cart.cartItems, newItem]
-      
-      return {...state,cart:{...state.cart,cartItems}}
-
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item._id === newItem._id
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            // Update item (if already in the cart)
+            item._id === existItem._id ? newItem : item
+          )
+        : // Add new item (if not in the cart)
+          [...state.cart.cartItems, newItem];
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
     default:
       return state;
   }
@@ -28,9 +39,9 @@ function reducer(state, action) {
 
 function StoreProvider(props) {
   const [state, dispatch] = React.useReducer(reducer, initialState);
-  // useReducer returns state and dispatch 
+  // useReducer returns state and dispatch
   const value = { state, dispatch };
   return <Store.Provider value={value}>{props.children}</Store.Provider>;
 }
 
-export {Store, StoreProvider}
+export { Store, StoreProvider };
--- frontend/src/pages/CartScreen.js
+++ frontend/src/pages/CartScreen.js
@@ -4,16 +4,38 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { Store } from '../Store';
 import MessageBox from '../components/MessageBox';
+import axios from 'axios';
 
 export default function CartScreen() {
+  const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = React.useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  async function updateCartHandler(item, quantity) {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  }
+
+  function removeItemHandler(item) {
+    ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  }
+
+  function checkoutHandler() {
+    navigate('/signin?redirect=/shipping');
+  }
 
   return (
     <div>
@@ -41,17 +63,32 @@ export default function CartScreen() {
                       <Link to={`/product/${item.slug}`}>{item.name} </Link>
                     </Col>
                     <Col md={3}>
-                      <Button variant="light" disabled={item.quantity === 1}>
+                      <Button
+                        variant="light"
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity - 1)
+                        }
+                        disabled={item.quantity === 1}
+                      >
                         <i className="fas fa-minus-circle"></i>
                       </Button>
                       <span> {item.quantity} </span>
-                      <Button variant="light">
+                      <Button
+                        variant="light"
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity + 1)
+                        }
+                        disabled={item.quantity === item.countInStock}
+                      >
                         <i className="fas fa-plus-circle"></i>
                       </Button>
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant="light">
+                      <Button
+                        variant="light"
+                        onClick={() => removeItemHandler(item)}
+                      >
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -78,6 +115,7 @@ export default function CartScreen() {
                       type="button"
                       variant="primary"
                       disabled={cartItems.length === 0}
+                      onClick={checkoutHandler}
                     >
                       Proceed to Checkout
                     </Button>
