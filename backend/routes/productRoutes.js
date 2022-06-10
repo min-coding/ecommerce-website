62849cd4f95b4e30ffecaebb0e018d08e0b3d703
Min <85483551+min-coding@users.noreply.github.com>
2022-06-10 16:47:37 +0700

    connect data to Mongodb

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import Product from '../models/productModel.js';
+
+const productRouter = express.Router();
+
+productRouter.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+productRouter.get('/slug/:slug', async (req, res) => {
+  /**
+   *  checking if there's any slug in the database matches the slug requested from
+      client side
+   */
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+productRouter.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+export default productRouter;
